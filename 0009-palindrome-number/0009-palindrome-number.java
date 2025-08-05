b381927e2fb3f818d54279c6db75021c93c75b86
Advithiya v <advithyav03@gmail.com>
2025-08-06 00:35:04 +0530

    Time: 5 ms (65.91%), Space: 43.9 MB (88.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int original = x;
+        int result=0;
+        while(x>0){
+            result =result*10+x%10;
+            x=x/10;
+        }
+        if(result==original){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
