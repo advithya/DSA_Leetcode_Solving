f79885163565b828c88f551c0c72bf3435d34938
Advithiya v <advithyav03@gmail.com>
2026-02-12 15:42:59 +0530

    Time: 4 ms (80.15%), Space: 44.4 MB (75.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        int charCount[] = new int[256];
+
+        for(int i=0; i<s.length();i++){
+            charCount[s.charAt(i)]++;
+            charCount[t.charAt(i)]--;
+        }
+        for(int i=0;i<charCount.length;i++){
+            if(charCount[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
